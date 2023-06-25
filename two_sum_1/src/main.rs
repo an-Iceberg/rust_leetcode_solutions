5fd4304c2594521d0cb91e63de4bcc8b9394166d
an-Iceberg <a.g.marosi@bluewin.ch>
2023-06-25 11:19:38 +0200

    Added some more tests

--- two_sum_1/src/main.rs
+++ two_sum_1/src/main.rs
@@ -10,12 +10,8 @@ fn two_sum(numbers: Vec<i32>, target: i32) -> Option<(usize, usize)> {
     for (second_index, _) in numbers
       .iter()
       .enumerate()
-      .skip(first_index)
+      .skip(first_index + 1)
     {
-      if first_index == second_index {
-        continue;
-      }
-
       if numbers[first_index] + numbers[second_index] == target {
         return Some((first_index, second_index));
       }
@@ -57,5 +53,26 @@ mod tests {
     let numbers = vec![1, 2];
     let target = 6;
     assert_eq!(two_sum(numbers, target), None);
+  }
+
+  #[test]
+  fn no_match_2() {
+    let numbers = vec![2, 4, 6, 8, 10, 12];
+    let target = 17;
+    assert_eq!(two_sum(numbers, target), None);
+  }
+
+  #[test]
+  fn match_1() {
+    let numbers = vec![2, 5, 6, 7, 11, 12];
+    let target = 23;
+    assert_eq!(two_sum(numbers, target), Some((4, 5)));
+  }
+
+  #[test]
+  fn unsorted_list_1() {
+    let numbers = vec![12, 6, 7, 5, 11, 2];
+    let target = 23;
+    assert_eq!(two_sum(numbers, target), Some((0, 4)));
   }
 }
