94afd9e46e3e81dbdf8717e86e7267de245c2137
an-Iceberg <a.g.marosi@bluewin.ch>
2023-06-26 19:10:06 +0200

    Added tests for #66; Adjusted launch option for debug

--- plus_one_66/src/main.rs
+++ plus_one_66/src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-  println!("Hello, world!");
+fn main() {}
+
+// [Problem](https://leetcode.com/problems/plus-one/)
+// #[allow(dead_code)]
+fn plus_one(digits: &mut Vec<i32>) {
+  todo!()
+}
+
+#[cfg(test)]
+mod tests {
+  use crate::plus_one;
+
+  // Examples from leetcode
+
+  #[test]
+  fn example_1() {
+    let mut digits: Vec<i32> = vec![1, 2, 3];
+    plus_one(&mut digits);
+    assert_eq!(digits, vec![1, 2, 4]);
+  }
+
+  #[test]
+  fn example_2() {
+    let mut digits: Vec<i32> = vec![4, 3, 2, 1];
+    plus_one(&mut digits);
+    assert_eq!(digits, vec![4, 3, 2, 2]);
+  }
+
+  #[test]
+  fn example_3() {
+    let mut digits: Vec<i32> = vec![9];
+    plus_one(&mut digits);
+    assert_eq!(digits, vec![1, 0]);
+  }
 }
