b924bbe40a830de0c08bbd2d8fdc3d3c4a6e0779
an-Iceberg <a.g.marosi@bluewin.ch>
2023-06-27 10:43:57 +0200

    Solved #66

--- plus_one_66/src/main.rs
+++ plus_one_66/src/main.rs
@@ -1,9 +1,42 @@
 fn main() {}
 
 // [Problem](https://leetcode.com/problems/plus-one/)
-// #[allow(dead_code)]
+#[allow(dead_code)]
 fn plus_one(digits: &mut Vec<i32>) {
-  todo!()
+  let mut carry = false;
+
+  // Increment last digit (with carry if applicable)
+  let last_index = digits.len() - 1;
+  digits[last_index] += 1;
+
+  if digits[last_index] > 9 {
+    carry = true;
+    digits[last_index] = 0;
+  } else {
+    return;
+  }
+
+  // Increment other digits as long as they create carries
+  for i in (0..digits.len() - 1).rev() {
+    if carry {
+      digits[i] += 1;
+      carry = false;
+    }
+
+    if digits[i] > 9 {
+      carry = true;
+      digits[i] = 0;
+    }
+
+    if !carry {
+      break;
+    }
+  }
+
+  // If first digit creates carry, prepend a 1
+  if carry {
+    digits.insert(0, 1);
+  }
 }
 
 #[cfg(test)]
@@ -31,5 +64,14 @@ mod tests {
     let mut digits: Vec<i32> = vec![9];
     plus_one(&mut digits);
     assert_eq!(digits, vec![1, 0]);
+  }
+
+  // Own tests
+
+  #[test]
+  fn test_for_9999() {
+    let mut digits: Vec<i32> = vec![9, 9, 9, 9];
+    plus_one(&mut digits);
+    assert_eq!(digits, vec![1, 0, 0, 0, 0]);
   }
 }
