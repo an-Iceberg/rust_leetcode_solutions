b3cd97cc6e777c3e7e95d3b31a128a0756cc0542
an-Iceberg <a.g.marosi@bluewin.ch>
2023-08-20 15:26:08 +0200

    Added solution to #202

--- happy_number_202/src/main.rs
+++ happy_number_202/src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-  println!("Hello, world!");
+#[cfg(test)]
+mod tests;
+
+fn main() {}
+
+// [Problem](https://leetcode.com/problems/happy-number/)
+fn is_happy(number: u32) -> bool
+{ let mut number = number;
+  let mut previous_numbers = vec![];
+
+  loop
+  { number = number.to_string()
+      .chars()
+      .map(|number| number.to_digit(10).unwrap())
+      .map(|digit| digit.pow(2))
+      .sum::<u32>();
+
+    // TODO: analyse sequence for patterns (4 seems to always show up)
+
+    if number != 1
+    { if previous_numbers.contains(&number)
+      { return false;
+      }
+      previous_numbers.push(number);
+    } else
+    { return true;
+    }
+  }
 }
---
+++ happy_number_202/src/tests.rs
@@ -0,0 +1,17 @@
+// Leetcode examples
+
+use crate::is_happy;
+
+#[test]
+fn example_1()
+{ assert!(is_happy(19));
+}
+
+#[test]
+fn example_2()
+{ assert!(!is_happy(2));
+}
+
+// Own tests
+
+// TODO: more tests
